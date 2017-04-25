62f0cd03c09ea31416ea8149f1caba13533da46b
KurumiSerori <hakureicode@gmail.com>
2017-04-25 18:24:27 +0900

    onQQMessage
    adding reaction to qq message

--- qqbot-task.py
+++ qqbot-task.py
@@ -1,5 +1,14 @@
 from qqbot import QQBotSched as qqbotsched, RunBot
+from qqbot import QQBotSlot as qqbotslot, RunBot
 import GiftCode
+
+@qqbotslot
+def onQQMessage(bot, contact, member, content):
+    if content == 'ml':
+        bot.SendTo(contact, GiftCode.GetCode())
+    elif content == '-stop':
+        bot.SendTo(contact, 'Closing')
+        bot.Stop()
 
 @qqbotsched(hour='12', minute='05')
 def mytask(bot):
